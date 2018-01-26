5590a77c93413c2d20391a4dd7c5704861478120
viaviare <viaviare@yandex.ru>
2018-01-26 19:46:54 +0300

    extract if_fill_field_value

--- fixture/group.py
+++ fixture/group.py
@@ -8,6 +8,52 @@ class GroupHelper:
         self.app = app
 
 
+    def fill_firm(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        # fill group firm
+        wd = self.app.wd
+        wd.find_element_by_id("content").click()
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click().clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group(wd)
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group(wd)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+
+    def select_first_group(self, wd):
+        wd.find_element_by_name("selected[]").click()
+
+
     def open_groups_page(self):
         wd = self.app.wd
         # open groups page
@@ -18,32 +64,3 @@ class GroupHelper:
         wd = self.app.wd
         # return to groups page
         wd.find_element_by_link_text("group page").click()
-
-
-
-    def fill_firm(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group firm
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def delete_first_group(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
