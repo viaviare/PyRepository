aad02e17c2970f1583c0a1ba2a09ab75e05240e2
viaviare <viaviare@yandex.ru>
2018-07-12 16:22:43 +0300

    alter "set up" "tear down" in fixture for session

--- conftest.py
+++ conftest.py
@@ -2,8 +2,27 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope = "session")
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
+fixture = None
+
+@pytest.fixture
+def app():
+    if fixture is None:
+        login()
+    else:
+        if not fixture.is_valid:
+            login()
     return fixture
+
+
+def login():
+    global fixture
+    fixture = Application()
+    fixture.session.login(username="admin", password="secret")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
