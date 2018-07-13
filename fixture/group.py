63ee3cea1fe0b37a62563f85da7d3b1a02821103
viaviare <viaviare@yandex.ru>
2018-07-13 14:03:29 +0300

    add optimization in tests

--- fixture/group.py
+++ fixture/group.py
@@ -8,7 +8,7 @@ class GroupHelper:
         self.app = app
 
 
-    def fill_firm(self, group):
+    def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
         # init group creation
@@ -20,26 +20,18 @@ class GroupHelper:
 
 
     def fill_group_form(self, group):
-        # fill group firm
         wd = self.app.wd
         wd.find_element_by_id("content").click()
-        self.change_field_value("group_name", group.name)
-        self.change_field_value("group_header", group.header)
-        self.change_field_value("group_footer", group.footer)
+        self.app.helper.type("group_name", group.name)
+        self.app.helper.type("group_header", group.header)
+        self.app.helper.type("group_footer", group.footer)
 
-
-    def change_field_value(self, field_name, text):
-        wd = self.app.wd
-        if text is not None:
-            wd.find_element_by_name(field_name).click()
-            wd.find_element_by_name(field_name).clear()
-            wd.find_element_by_name(field_name).send_keys(text)
 
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group(wd)
+        self.select_first_group()
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
@@ -47,24 +39,23 @@ class GroupHelper:
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group(wd)
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
 
-    def select_first_group(self, wd):
+    def select_first_group(self):
+        wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
 
     def open_groups_page(self):
         wd = self.app.wd
-        # open groups page
         wd.find_element_by_link_text("groups").click()
 
 
     def return_to_groups_page(self):
         wd = self.app.wd
-        # return to groups page
         wd.find_element_by_link_text("group page").click()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,9 @@ from model.group import Group
 
     
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.fill_firm(Group(name="dfsdf", header="vbnvbn", footer="qqqwewe"))
-    app.session.logout()
+    app.group.create(Group(name="dfsdf", header="vbnvbn", footer="qqqwewe"))
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.fill_firm(Group(name="", header="", footer=""))
-    app.session.logout()
+    app.group.create(Group(name="", header="", footer=""))
 
