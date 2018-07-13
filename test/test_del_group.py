a04ae51acaa6ce1bb578a24e0b92b0be48d4635e
viaviare <viaviare@yandex.ru>
2018-07-13 21:52:37 +0300

    add hash optimization in tests

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,13 +2,14 @@ from model.group import Group
 
 
 def test_delete_group(app):
-    if app.group.count == 0:
+    if app.group.count() == 0:
         app.group.create(Group(name="temp"))
 
+    index=0
     old_groups = app.group.get_group_list()
-    app.group.delete()
+    app.group.delete(index)
+    assert len(old_groups) - 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
-    old_groups[0:1] = []
+    old_groups[index:index+1] = []
     assert old_groups == new_groups
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    if app.group.count == 0:
+    if app.group.count() == 0:
         app.group.create(Group(name="temp"))
 
     index = 0
@@ -10,8 +10,8 @@ def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
     group.id = old_groups[index].id
     app.group.modify(index, group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
