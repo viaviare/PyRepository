6f0575437d58ede5332089463d6343b300745a3c
viaviare <viaviare@yandex.ru>
2018-07-13 15:30:45 +0300

    add small optimization in helpers

--- fixture/group.py
+++ fixture/group.py
@@ -1,23 +1,30 @@
-
-
-
-
 class GroupHelper:
-
     def __init__(self, app):
         self.app = app
 
-
     def create(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
+        self.app.navigator.open_groups_page()
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
-        # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def modify(self, new_group_data):
+        wd = self.app.wd
+        self.app.navigator.open_groups_page()
+        self.select_first_group()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def delete(self):
+        wd = self.app.wd
+        self.app.navigator.open_groups_page()
+        self.select_first_group()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -26,35 +33,9 @@ class GroupHelper:
         self.app.helper.type("group_header", group.header)
         self.app.helper.type("group_footer", group.footer)
 
-
-
-    def delete_first_group(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        self.select_first_group()
-        wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
-
-
-    def modify_first_group(self, new_group_data):
-        wd = self.app.wd
-        self.open_groups_page()
-        self.select_first_group()
-        wd.find_element_by_name("edit").click()
-        self.fill_group_form(new_group_data)
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
-
-
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
-
-
-    def open_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
-
 
     def return_to_groups_page(self):
         wd = self.app.wd
--- fixture/navigation.py
+++ fixture/navigation.py
@@ -1,14 +1,18 @@
-
-
 class NavigationHelper:
-
-
     def __init__(self, app):
         self.app = app
 
-
     def open_start_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get("http://localhost/addressbook")
 
+    def open_main_page(self):
+        wd = self.app.wd
+        if wd.current_url == "http://localhost/addressbook" and len(wd.find_elements_by_link_text("Logout")) > 0:
+            return
+        wd.find_element_by_link_text("home").click()
 
+    def open_groups_page(self):
+        wd = self.app.wd
+        if not wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("edit")) > 0:
+            wd.find_element_by_link_text("groups").click()
